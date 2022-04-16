c867157d87d8a17448a08e693c3d646f3c3ff4a3
konstante <konstantechang@gmail.com>
2022-04-16 18:10:17 +0800

    add 第二種create方法-章節:另一種新增方法

--- server.js
+++ server.js
@@ -7,28 +7,53 @@ mongoose.connect('mongodb://localhost:27017/hotel').then( () => {
     console.log(error.reason);
 } );
 
-const roomSchema = {
+const roomSchema = new mongoose.Schema({
     name:String,
     price: {
         type:Number,
         required: [true, "價格必填"]
     },
     rating: Number,
-}
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        select: false,
+    }
+}, 
+{
+    versionKey:false
 
-const Room = mongoose.model('Room', roomSchema);//小小特性  collection Room 進 mongoDB 會強制小寫 room,強制加s --> rooms
-
-const testRoom = new Room({
-    name: "總統洧杰單人房",
-    price: 2000,
-    rating: 4.5
 });
 
-testRoom.save().then( () => {
-    console.log("新增資料成功")
-} ).catch(error => {
-    console.log(error);
-})
+const Room = mongoose.model('Room',
+ roomSchema);//小小特性  collection Room 進 mongoDB 會強制小寫 room,強制加s --> rooms
+
+//第二種create法 
+
+Room.create({
+    name: "總統超級單人房2",
+    price: 2000,
+    rating: 4.5,
+}).then( () => {
+    console.log("資料寫入成功");
+} ).catch( error => {
+    console.log(error.errors);
+} );
+
+
+
+//第一種create法:  new + save 
+// const testRoom = new Room({
+//     name: "總統洧杰單人房5",
+//     price: 2100,
+//     rating: 4.5
+// });
+
+// testRoom.save().then( () => {
+//     console.log("新增資料成功")
+// } ).catch(error => {
+//     console.log(error);
+// })
 
 const requestListener = (req, res) => {
     console.log(req.url);
