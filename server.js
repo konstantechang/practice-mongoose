39a13a3cc35e6a943753976b0433f2b07aff3d35
konstante <konstantechang@gmail.com>
2022-04-16 12:48:54 +0800

    add modified server.js

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ const roomSchema = {
 
 const Room = mongoose.model('Room', roomSchema);//小小特性  collection Room 進 mongoDB 會強制小寫 room,強制加s --> rooms
 
+const testRoom = new Room({
+    name: "總統洧杰單人房",
+    price: 2000,
+    rating: 4.5
+});
+
+testRoom.save().then( () => {
+    console.log("新增資料成功")
+} ).catch(error => {
+    console.log(error);
+})
+
 const requestListener = (req, res) => {
     console.log(req.url);
     res.end();
